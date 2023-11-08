cee1b7d5a948a1f8db97a187cd9cc5182d8b3295
s3rxio <cergotabagua222@gmail.com>
2023-11-08 08:39:20 +0300

    Fix apps install and latte dock

--- install_apps.sh
+++ install_apps.sh
@@ -1,13 +1,14 @@
-pacman -S obs-studio firefox discord telegram-desktop latte-dock
+sudo pacman -S obs-studio firefox discord telegram-desktop latte-dock
 
 mkdir -p ~/.config/autostart
 cp /usr/share/applications/org.kde.latte-dock.desktop ~/
-mv org.kde.latte-dock.desktop ~/.config/autostart
+mv ~/org.kde.latte-dock.desktop ~/.config/autostart
 
 sudo git clone https://aur.archlinux.org/yay-git.git
 sudo chown -R $USER:$USER yay-git
 cd yay-git
 makepkg -si
+cd ..
 sudo rm -rf yay-git
 
 yay -S visual-studio-code-bin git-flow
