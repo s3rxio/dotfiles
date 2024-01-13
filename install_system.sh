3d3ad973fb3739c4ed98a3c7614b1028ca249d6d
s3rxio <cergotabagua222@gmail.com>
2024-01-13 21:09:22 +0300

    Did system installation without logging into the root shell

--- install_system.sh
+++ install_system.sh
@@ -1,14 +1,14 @@
-pacman -Syu vim git plasma konsole dolphin spectacle ark vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager spectacle
+sudo pacman -Syu vim git plasma konsole dolphin spectacle ark vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager spectacle
 
 git config --global core.editor "vim"
 
-pacman -S sddm sddm-kcm
-mkdir /etc/sddm.conf.d/
-echo "[Autologin]
+sudo pacman -S sddm sddm-kcm
+sudo mkdir /etc/sddm.conf.d/
+sudo sh -c "echo \"[Autologin]
 User=s3rxio
-Session=plasma" > /etc/sddm.conf.d/autologin.conf
-systemctl enable sddm
+Session=plasma\" > /etc/sddm.conf.d/autologin.conf"
+sudo systemctl enable sddm
 
-pacman -S linux-headers nvidia
-sed -e "s/kms //g" -i /etc/mkinitcpio.conf
-mkinitcpio -P
+sudo pacman -S linux-headers nvidia
+sudo sed -e "s/kms //g" -i /etc/mkinitcpio.conf
+sudo mkinitcpio -P
