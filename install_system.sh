01e3c62a7b07e2705d0a224436ef39a738869790
s3rxio <cergotabagua222@gmail.com>
2024-03-30 22:56:54 +0300

    refactor(sddm): Moved the config to a file

--- install_system.sh
+++ install_system.sh
@@ -1,12 +1,10 @@
-sudo pacman -Syu vim git htop plasma konsole dolphin spectacle ark vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager networkmanager-openvpn
+sudo pacman -Syu vim git htop plasma konsole dolphin spectacle ark gwenview vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager networkmanager openvpn libxcb xorg-xwayland
 
 git config --global core.editor "vim"
 
 sudo pacman -S sddm sddm-kcm
 sudo mkdir /etc/sddm.conf.d/
-sudo sh -c "echo \"[Autologin]
-User=s3rxio
-Session=plasma\" > /etc/sddm.conf.d/autologin.conf"
+sudo mv ./sddm/* /etc/sddm.conf.d/
 sudo systemctl enable sddm
 
 sudo pacman -S linux-headers nvidia
