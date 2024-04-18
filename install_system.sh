b1d3c759b294f92ae39c1e4f2ed53f1e36e1e46f
s3rxio <cergotabagua222@gmail.com>
2024-04-18 17:24:08 +0300

    feat: add config files from /etc

--- install_system.sh
+++ install_system.sh
@@ -1,12 +1,18 @@
-sudo pacman -Syu vim git htop plasma konsole dolphin spectacle ark gwenview vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager networkmanager openvpn libxcb xorg-xwayland
+sudo cp ./etc/pacman.conf /etc/
+sudo pacman -Syu vim git htop plasma konsole dolphin spectacle ark gwenview vlc bluez bluez-utils pulseaudio pulseaudio-alsa pulseaudio-bluetooth plasma-pa kwallet-pam kwalletmanager networkmanager openvpn libxcb os-prober
+
+sudo systemctl enable NetworkManager
+
+sudo cp ./etc/environment /etc/
+sudo cp ./etc/default/grub /etc/default/
+sudo grub-mkconfig -o /boot/grub/grub.cfg
 
 git config --global core.editor "vim"
 
 sudo pacman -S sddm sddm-kcm
-sudo mkdir /etc/sddm.conf.d/
-sudo mv ./sddm/* /etc/sddm.conf.d/
+sudo cp -r ./etc/sddm.conf.d/ /etc/
 sudo systemctl enable sddm
 
 sudo pacman -S linux-headers nvidia
-sudo sed -e "s/kms //g" -i /etc/mkinitcpio.conf
+sudo cp ./etc/mkinitcpio.conf /etc/
 sudo mkinitcpio -P
